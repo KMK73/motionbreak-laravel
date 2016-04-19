70f3ae97176802ef51d0ec80740bdceb15f90159
KMK73 <kmk73@miami.edu>
2016-04-19 14:39:50 -0400

    added completed breaks command file manually

---
+++ app/Console/Commands/ResetCompletedBreaks.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\User; //user model
+use App\UserLocation; //user location model
+use App\CompletedMovement; //completed movement model
+use App\UserBreak; //user breaks model
+use App\Jobs\Job;
+
+class ResetCompletedBreaks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:breaks';
+    //protected $signature = 'notification:test';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset completed movements nightly.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    
+    
+    public function __construct()
+    {
+        //pass in the user that launched this job through the controller
+//        $this->user = $user; 
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $movementSettings = CompletedMovement::all();
+        $movementSettings->completed_breaks = 0;
+        $movementSettings->save();
+                
+        return response()->json($movementSettings); 
+        
+    }//end of handle function
+}

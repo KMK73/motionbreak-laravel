72e844045285d625ee7ee2949bd0a91bbbeaf4ac
KMK73 <kmk73@miami.edu>
2016-04-19 22:11:17 -0400

    update

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -174,7 +174,7 @@ movementUUID - gets all movements for that one user
     public function completedMovementUUID($uuid) {
         $movements = CompletedMovement::where('uuid', '=', $uuid)->get();;
         //filter by date
-        $movements->whereDay('created_at', '=', date('d'));
+        $movements->whereDate('created_at', '=', Carbon::today()->toDateString());
         return response()->json(array('movements' => $movements));
     }
     

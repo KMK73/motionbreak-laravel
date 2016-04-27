6194101579aba63b073fe2b331be6570f54e880c
KMK73 <kmk73@miami.edu>
2016-04-27 19:23:06 -0400

    edit

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -647,8 +647,8 @@ COMPLETED BREAKS ROUTES
         $user = User::find($user_id);
          
         //check if notifications table has multiple jobs and delete those job ids from jobs table 
-        $notifications = Notification::all();
-        $jobID = $notfications->value('job_id');
+        $notifications = Notification::where('user_id', '=', $user_id)->get();
+        //$jobID = $notfications->value('job_id');
 //        $notifications = Notification::where('user_id', '=', $user_id)->value('job_id')->get();
         
         echo "notifications last job id: " . $jobID . "\n"; 
